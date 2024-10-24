a9f90c527801ddd29e4185ced7a09adba04e6ccc
rafco <rafco@LOBO>
2024-10-24 07:09:28 -0400

    nav_autenticado

--- frontend/src/app/components/nav/nav.component.ts
+++ frontend/src/app/components/nav/nav.component.ts
@@ -8,23 +8,26 @@ import { AuthService } from '../../services/auth.service'; // Asegúrate de impo
   styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent  implements OnInit {
-  usuario: any; // Para almacenar la información del usuario
+  usuario: any; 
+  isAuthenticated: boolean = false;
 
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
     this.authService.getUser().subscribe(
       (response) => {
-        this.usuario = response; // Guardar la información del usuario
+        this.usuario = response; 
       },
       (error) => {
         console.error('Error al obtener el usuario:', error);
       }
     );
 
+    const token = localStorage.getItem('token');
+    this.isAuthenticated = !!token; 
+
   }
 
-  // Método que se llamará al hacer clic en el botón de cerrar sesión
   logout() {
     this.authService.logout();
     //window.location.reload();
--- frontend/src/app/components/slide-menu/slide-menu.component.ts
+++ frontend/src/app/components/slide-menu/slide-menu.component.ts
@@ -18,9 +18,8 @@ export class SlideMenuComponent implements OnInit {
       this.userRole = parseInt(storedRoleId, 10);
     }
 
-
-     // Verifica si el token de autenticación está presente en el localStorage
-     const token = localStorage.getItem('token');
-     this.isAuthenticated = !!token; // Se convierte en `true` si hay un token, `false` si no.
+    // Verifica si el token de autenticación está presente en el localStorage
+    const token = localStorage.getItem('token');
+    this.isAuthenticated = !!token; // Se convierte en `true` si hay un token, `false` si no.
   }
 }
