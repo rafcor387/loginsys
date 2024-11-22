747343f22193826b0fcde0c719e95941d7f4280b
rafco <rafco@LOBO>
2024-11-22 17:11:44 -0400

    cosas

--- backend/app/Http/Controllers/CategoriaController.php
+++ backend/app/Http/Controllers/CategoriaController.php
@@ -31,14 +31,14 @@ class CategoriaController extends Controller
                 'descripcion' => [
                     'nullable',
                     'string',
-                    'max:200',
+                    'max:300',
                     //'not_regex:/^\s*$/' // No permite solo espacios en blanco
                 ]
             ], [
                 'nombre.required' => 'El campo de nombre es obligatorio.',
                 'nombre.regex' => 'El nombre solo debe contener letras, números, espacios y puntos.',
                 'nombre.max' => 'Solo se permite hasta maximo 30 caracteres en el campo nombre.',
-                'descripcion.max' => 'La descripción no debe exceder los 200 caracteres.',
+                'descripcion.max' => 'La descripción no debe exceder los 300 caracteres.',
                 //'not_regex' => 'El campo no debe contener solo espacios en blanco.'
             ]);
 
@@ -67,14 +67,14 @@ class CategoriaController extends Controller
                 'descripcion' => [
                     'nullable',
                     'string',
-                    'max:200',
+                    'max:300',
                     //'not_regex:/^\s*$/'
                 ]
             ], [
                 'nombre.required' => 'El campo de nombre es obligatorio.',
                 'nombre.regex' => 'El nombre solo debe contener letras, números, espacios y puntos.',
                 'nombre.max' => 'Solo se permite hasta maximo 30 caracteres en el campo nombre.',
-                'descripcion.max' => 'La descripción no debe exceder los 200 caracteres.',
+                'descripcion.max' => 'La descripción no debe exceder los 300 caracteres.',
                 //'not_regex' => 'El campo no debe contener solo espacios en blanco.'
             ]);
             $validatedData['nombre'] = strtoupper($validatedData['nombre']);
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class AuthService {
   //private apiUrl = 'http://project.test/backend/public/api'; // Cambia esto si es necesarios
   private apiUrl = 'https://izzicode-production.up.railway.app/api'; // Cambia esto si es necesarios
+  //[src]="'http://project.test/backend/public/storage/'  + repuesto.imagen"
   //private apiUrl = 'http://proyecto.test/Izzi_Code/backend/public/api'; // Cambia esto si es necesario
   //private apiUrl = 'http://127.0.0.1:8000/api'; // Cambia esto si es necesarios
   //private apiUrl = 'http://proyecto2.test/loginsys/backend/public/api'; // Cambia esto si es necesarios
