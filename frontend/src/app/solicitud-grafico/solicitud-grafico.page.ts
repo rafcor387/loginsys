d552d4168a2883e981d3e377843972d09008d685
MrJonasdKa <jonas.bloody.bl@gmail.com>
2024-11-23 20:25:33 -0400

    Regresion Final?

--- frontend/src/app/solicitud-grafico/solicitud-grafico.page.ts
+++ frontend/src/app/solicitud-grafico/solicitud-grafico.page.ts
@@ -12,8 +12,7 @@ export class SolicitudGraficoPage {
   anio: number = 0;
   errorMessage: string = ''; // Variable para mostrar el mensaje de error
   years: number[] = []; // Array para almacenar los años
-
-  dataProcessed: boolean = false; // para verificar si los datos ya existen en la tabla de regresion
+  dataProcessed: boolean = false; // Track if data has been processed
 
   constructor(private http: HttpClient, private router: Router) {
     this.generateYears(); // Generate years on component initialization
@@ -32,24 +31,29 @@ export class SolicitudGraficoPage {
     this.errorMessage = ''; // Reset error message before submission
     this.dataProcessed = false; // Reset data processed flag
     if (!this.idRepuesto || !this.anio) {
-        this.errorMessage = 'Por favor complete todos los campos antes de enviar.';
-        return;
+      this.errorMessage = 'Por favor complete todos los campos antes de enviar.';
+      return;
     }
 
     const data = { id_repuesto: this.idRepuesto, anio: this.anio };
 
     this.http.post('http://127.0.0.1:8000/api/llenar-test-reg', data).subscribe(
       (response: any) => {
-          // Check if the response indicates data has been processed
-          if (response.message) {
-              this.dataProcessed = true; // Set flag to true
-              this.errorMessage = response.message; // Show the message
-          }
+        // Check if the response indicates data has been processed
+        if (response.message) {
+          this.dataProcessed = true; // Set flag to true
+          this.errorMessage = response.message; // Show the message
+
+          // Set a timer to redirect after 5 seconds
+          setTimeout(() => {
+            this.router.navigate([`/sales-prediction/${this.idRepuesto}`]);
+          }, 5000); // 5000 milliseconds = 5 seconds
+        }
       },
       (error) => {
-          // Handle error as before
-          console.error('Error al procesar los datos:', error);
-          this.errorMessage = error.error.message || 'Hubo un error al procesar los datos. Intente de nuevo.';
+        // Handle error as before
+        console.error('Error al procesar los datos:', error);
+        this.errorMessage = error.error.message || 'Hubo un error al procesar los datos. Intente de nuevo.';
       }
     );
   }
