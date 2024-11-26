23d8820b56b1940f74ad7a3744164702cffd1929
Sergio Poma Chuquimia <serospoma@gmail.com>
2024-11-26 06:50:55 -0400

    im3

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -9,8 +9,8 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
   //private apiUrl = 'http://project.test/backend/public/api'; // Cambia esto si es necesarios
-  //private apiUrl = 'https://izzicode-production.up.railway.app/api';
-  private apiUrl = 'http://127.0.0.1:8000/api'; 
+  private apiUrl = 'https://izzicode-production.up.railway.app/api';
+  //private apiUrl = 'http://127.0.0.1:8000/api'; 
   //[src]="'https://izzicode-production.up.railway.app/storage/'  + repuesto.imagen"
   //[src]="'http://project.test/backend/storage/app/public/'  + repuesto.imagen"
   //[src]="'http://project.test/backend/public/storage/'  + repuesto.imagen"
